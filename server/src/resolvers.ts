95d46b3cdf97cb19ded9b4bbfd4dd99837c314db
miracoly <68049792+miracoly@users.noreply.github.com>
2024-06-24 10:08:32 +0200

    feat(server): add resolver for module

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,23 +1,28 @@
-import { Resolvers } from "./types"
+import {Resolvers} from "./types"
 
 export const resolvers: Resolvers = {
   Query: {
     // returns an array of Tracks that will be used to populate the homepage grid of our web client
-    tracksForHome: (_, __, { dataSources }) => {
+    tracksForHome: (_, __, {dataSources}) => {
       return dataSources.trackAPI.getTracksForHome();
     },
 
     // get a single track by ID, for the track page
-    track: (_, { id }, { dataSources }) => {
+    track: (_, {id}, {dataSources}) => {
       return dataSources.trackAPI.getTrack(id);
     },
+
+    // get a single module by ID, for the module page
+    module: (_, {id}, {dataSources}) => {
+      return dataSources.trackAPI.getModule(id);
+    }
   },
   Track: {
-    author: ({ authorId }, _, { dataSources }) => {
+    author: ({authorId}, _, {dataSources}) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
 
-    modules: ({ id }, _, { dataSources }) => {
+    modules: ({id}, _, {dataSources}) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
   },
